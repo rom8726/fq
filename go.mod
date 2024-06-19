931eb5fec1af61195be2385ecf26cc7a5f8448d6
Roman Chudov <roman.chudov@gmail.com>
2024-06-19 04:32:09 +0300

    config package.

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module fq
 go 1.22
 
 require (
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.9.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -14,5 +16,4 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/config/config.go
@@ -0,0 +1,92 @@
+// Package config is a configuration package
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	configDefaultFilePath = "config.yml"
+)
+
+type Config struct {
+	Engine  *EngineConfig  `yaml:"engine"`
+	WAL     *WALConfig     `yaml:"wal"`
+	Network *NetworkConfig `yaml:"network"`
+	Logging *LoggingConfig `yaml:"logging"`
+}
+
+//nolint:tagliatelle // it's ok
+type NetworkConfig struct {
+	Address        string        `yaml:"address"`
+	MaxConnections int           `yaml:"max_connections"`
+	MaxMessageSize string        `yaml:"max_message_size"`
+	IdleTimeout    time.Duration `yaml:"idle_timeout"`
+}
+
+type LoggingConfig struct {
+	Level  string `yaml:"level"`
+	Output string `yaml:"output"`
+}
+
+type EngineConfig struct {
+	Type string `yaml:"type"`
+}
+
+type WALConfig struct {
+	FlushingBatchLength  int           `yaml:"flushing_batch_length"`
+	FlushingBatchTimeout time.Duration `yaml:"flushing_batch_timeout"`
+	MaxSegmentSize       string        `yaml:"max_segment_size"`
+	DataDirectory        string        `yaml:"data_directory"`
+}
+
+func Init() (Config, error) {
+	var configPath string
+
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	} else {
+		configPath = configDefaultFilePath
+	}
+
+	info, err := os.Stat(configPath)
+	if err != nil {
+		return Config{}, fmt.Errorf("stat config %q: %w", configPath, err)
+	}
+
+	if info.IsDir() {
+		return Config{}, fmt.Errorf("config %q is a directory", configPath)
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return Config{}, fmt.Errorf("read config file: %w", err)
+	}
+
+	cfg := Config{}
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return Config{}, fmt.Errorf("unmarshal config: %w", err)
+	}
+
+	if err := validate(&cfg); err != nil {
+		return Config{}, fmt.Errorf("validate config: %w", err)
+	}
+
+	return cfg, nil
+}
+
+func validate(cfg *Config) error {
+	err := validation.ValidateStruct(cfg.Network,
+		validation.Field(&cfg.Network.Address, validation.Required),
+	)
+	if err != nil {
+		return fmt.Errorf("validate network section: %w", err)
+	}
+
+	return nil
+}
